076f3e64a23392d40ce02d909725864393fbf167
Trang <tmh327@drexel.edu>
2019-11-09 18:13:53 -0500

    Display classes, students in a classroom, classes and classroom/1... are not linked yet

--- project/Squads/views.py
+++ project/Squads/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from .models import Classroom
 
+
+def classroom(request, classroom_id):
+    current_classroom = get_object_or_404(Classroom, id=classroom_id)
+    context = {"students": current_classroom.student_ids}
+    return render(request, 'classroom.html', context)
+
+
+def classes(request):
+    context = {"classes": Classroom.objects.all()}
+    return render(request, 'classes.html', context)
 # Create your views here.
