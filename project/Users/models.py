74f076129a607c045a69358d5da8beeac8bc09c0
Lillian Dourlain <lcd64@drexel.edu>
2019-11-09 14:25:11 -0500

    update name

--- project/Users/models.py
+++ project/Users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class CustomUser(AbstractUser):
+class Administrator(AbstractUser):
     # to do: classrooms
     # classrooms = models.ManyToManyField()
     def __str__(self):
