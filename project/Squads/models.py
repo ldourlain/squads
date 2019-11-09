977c2b209d49300216f70d11384168163edafe20
Lillian Dourlain <lcd64@drexel.edu>
2019-11-09 18:57:12 -0500

    update models

--- project/Squads/models.py
+++ project/Squads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from Users.models import Administrator
 
 # Create your models here.
 
@@ -6,15 +7,16 @@ class Student(models.Model):
     full_name = models.CharField(max_length=80)
 
 class Classroom(models.Model):
-    teacher_name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80)
+    teacher_name = models.ForeignKey(Administrator, on_delete=models.CASCADE)
     student_ids = models.ManyToManyField(Student)
-    
+
 class Group(models.Model):
     student_ids = models.ManyToManyField(Student)
-    classroom_id = models.ForeignKey('Classroom',on_delete=models.CASCADE)
+    classroom_id = models.ForeignKey(Classroom, on_delete=models.CASCADE)
     date = models.DateField(auto_now=True)
 
 class Absence(models.Model):
     student_ids = models.ManyToManyField(Student)
-    classroom_id = models.ForeignKey('Classroom',on_delete=models.CASCADE)
+    classroom_id = models.ForeignKey(Classroom, on_delete=models.CASCADE)
     date = models.DateField(auto_now=True)
