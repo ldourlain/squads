6d75d75f55672d11ecc3fc420a62b1e75a2fa6df
Lillian Dourlain <lcd64@drexel.edu>
2019-11-09 15:56:57 -0500

    Register Models on Admin Site

--- project/Squads/admin.py
+++ project/Squads/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Classroom)
+admin.site.register(Group)
+admin.site.register(Absence)
