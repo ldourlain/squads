9a93c2599ea70e939c4620c2fdc0ee0f499f9e7f
Trang <tmh327@drexel.edu>
2019-11-10 01:57:26 -0500

    Add new class works

---
+++ project/Squads/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Classroom, Student
+
+
+class AddClassForm(forms.ModelForm):
+    class Meta:
+        model = Classroom
+        fields = ('name',)
+
+#class AddClassStudent(forms.ModelForm):
+ #   class Meta:
+  #      model = Classroom
+   #     fields =
