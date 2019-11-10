b844b722c884ba470ad7277449aa6c4455cbf446
Trang <tmh327@drexel.edu>
2019-11-10 06:52:35 -0500

    change new-class.html to new_class.html

--- project/Squads/urls.py
+++ project/Squads/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('classroom/<int:classroom_id>/', views.classroom, name='classroom'),
-    path('new-class/', views.add_class, name='add_class'),
+    path('new_class/', views.add_class, name='add_class'),
     path('add_class_students/<int:classroom_id>/', views.add_class_student, name='add_class_student'),
     path('classes/', views.classes, name='classes'),
     path('dashboard/', views.dashboard, name='dashboard'),
